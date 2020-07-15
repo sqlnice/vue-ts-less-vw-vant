bc10dc0854046a0e53b535d17cfee860454beb5d
时启龙 <472252510@qq.com>
2020-07-15 16:19:08 +0800

    增加货币处理

--- src/assets/js/tool.ts
+++ src/assets/js/tool.ts
@@ -180,6 +180,32 @@ const tool = {
       });
     }
     return obj;
-  }
+  },
+  // 货币处理1
+  currency(value: any, decimals: number) {
+    /**
+    * value  金额
+    * currency 货币符号
+    * decimals  保留位数
+    */
+    const digitsRE = /(\d{3})(?=\d)/g;
+    value = parseFloat(value);
+    if (!isFinite(value) || (!value && value !== 0)) return value;
+    // currency = currency != null ? currency : '$'
+    decimals = decimals != null ? decimals : 0;
+    var stringified = Math.abs(value).toFixed(decimals);
+    var _int = decimals
+      ? stringified.slice(0, -1 - decimals)
+      : stringified;
+    var i = _int.length % 3;
+    var head = i > 0 ? (_int.slice(0, i) + (_int.length > 3 ? ',' : '')) : '';
+    var _float = decimals ? stringified.slice(-1 - decimals) : '';
+    var sign = value < 0 ? '-' : '';
+    return sign + head + _int.slice(i).replace(digitsRE, '$1,') + _float;
+  },
+  // 货币处理2
+  currency2(value: any) {
+    value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  },
 };
 export default tool;
