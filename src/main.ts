5ac1219c9f265a2250576fdc557d99937cce8a0b
时启龙 <472252510@qq.com>
2020-07-23 11:20:07 +0800

    增加移动端按钮active样式

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import router from "./router";
 import store from "./vuex/index";
 import 'vant/lib/index.css';
 import './assets/style/phone-reset.css';
+import './assets/style/base.css';
 
 
 Vue.config.productionTip = false;
